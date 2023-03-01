c085fe0cb6bf76876466cd0c420934ea068762f2
krina-soni <krinaadeshra@gmail.com>
2023-03-01 15:15:29 +0000

    new changes

--- src/test/java/com/automation/test/OrderPageTest.java
+++ src/test/java/com/automation/test/OrderPageTest.java
@@ -4,37 +4,37 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class OrderPageTest extends BaseTest {
-	@Test
-	public void verifyUserAddProducttocart() {
-		loginPage.openWebsite();
-		loginPage.doLogin();
-
-		Assert.assertTrue(homePage.isHomePageLoaded(), "Home page is not loaded");
-
-		String itemName = homePage.getItemName();
-		String itemPrice = homePage.getItemPrice();
-
-		homePage.clickOnAddToCartBtn();
-		homePage.isRemoveitemVisible();
-		homePage.clickOnCartIcon();
-
-		Assert.assertTrue(cartPage.isCartPageDisplayed(), "Cart page is not loaded");
-		Assert.assertTrue(cartPage.getItemName().equals(itemName), "Item name is not matching with home page");
-		Assert.assertTrue(cartPage.getItemPrice().equals(itemPrice), "Item price is not matching with home page");
-
-		cartPage.clickOnCheckoutIcon();
-		Assert.assertTrue(chkout1.isContinueDisplayed(), "Checkout page is not loaded");
-		chkout1.AddDetails("Krina", "soni", "FK28RE");
-
-		String chkitemName = chkout2.getcheckoutItemName();
-		String chkitemPrice = chkout2.getcheckoutItemPrice();
-		Assert.assertTrue(chkout2.ischeckoutOverviewDisplayed(), "Checkout overviw page is not loaded");
-		Assert.assertTrue(cartPage.getItemName().equals(chkitemName), "Item name is not matching with cart page");
-		Assert.assertTrue(cartPage.getItemPrice().equals(chkitemPrice), "Item price is not matching with cart page");
-
-		chkout2.clickOnFinishbutton();
-		Assert.assertTrue(chkout2.isOrderCompleteDisplayed(), "Order is not Placed");
-	}
+//	@Test
+//	public void verifyUserAddProducttocart() {
+//		loginPage.openWebsite();
+//		loginPage.doLogin();
+//
+//		Assert.assertTrue(homePage.isHomePageLoaded(), "Home page is not loaded");
+//
+//		String itemName = homePage.getItemName();
+//		String itemPrice = homePage.getItemPrice();
+//
+//		homePage.clickOnAddToCartBtn();
+//		homePage.isRemoveitemVisible();
+//		homePage.clickOnCartIcon();
+//
+//		Assert.assertTrue(cartPage.isCartPageDisplayed(), "Cart page is not loaded");
+//		Assert.assertTrue(cartPage.getItemName().equals(itemName), "Item name is not matching with home page");
+//		Assert.assertTrue(cartPage.getItemPrice().equals(itemPrice), "Item price is not matching with home page");
+//
+//		cartPage.clickOnCheckoutIcon();
+//		Assert.assertTrue(chkout1.isContinueDisplayed(), "Checkout page is not loaded");
+//		chkout1.AddDetails("Krina", "soni", "FK28RE");
+//
+//		String chkitemName = chkout2.getcheckoutItemName();
+//		String chkitemPrice = chkout2.getcheckoutItemPrice();
+//		Assert.assertTrue(chkout2.ischeckoutOverviewDisplayed(), "Checkout overviw page is not loaded");
+//		Assert.assertTrue(cartPage.getItemName().equals(chkitemName), "Item name is not matching with cart page");
+//		Assert.assertTrue(cartPage.getItemPrice().equals(chkitemPrice), "Item price is not matching with cart page");
+//
+//		chkout2.clickOnFinishbutton();
+//		Assert.assertTrue(chkout2.isOrderCompleteDisplayed(), "Order is not Placed");
+//	}
 
 	@Test
 	public void CheckoutValidtion() throws InterruptedException {
